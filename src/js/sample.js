273cda4eb218bf1cc16f6a2094f340550aabcfee
Michael Alt <mail@michael-alt.info>
2020-03-25 22:47:28 +0100

    Provide Fetch API sample

--- src/js/sample.js
+++ src/js/sample.js
@@ -1,2 +1,19 @@
-let sample = 'Template';
-console.log(sample);
+
+const meeting = document.getElementById('meeting');
+const attendeesTable = document.querySelector('article.sample tbody');
+
+fetch('/meetings/1?_embed=attendees')
+  .then(response => response.json())
+  .then(({ id, title, attendees }) => {
+    meeting.dataset['id'] = id;
+    meeting.querySelector('span').textContent = title;
+    attendees.forEach(elt => {
+      attendeesTable.insertAdjacentHTML('beforeend',
+        `<tr>
+          <td>${elt.id}</td>
+          <td>${elt.name}</td>
+          <td>${elt.email}</td>
+        </tr>`
+      )
+    });
+  });
